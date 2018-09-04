29818fdeb63424f909da50c33ffb61dc21434e39
Charizze <Austin.charizze@gmail.com>
2018-09-04 23:58:39 +1200

    New Updated Files

--- app/src/main/java/com/example/epantry/Login.java
+++ app/src/main/java/com/example/epantry/Login.java
@@ -1,9 +1,17 @@
+/**
+ * TODO: save new <User> signup in db, method to check login matches existing, method that blank fields make button disabled
+ * test method for creating new login and making sure method returns match
+ * test method for successful login to new activity?
+ * test method for any blank fields that button is disabled
+ */
+
 package com.example.epantry;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -16,23 +24,26 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i("Login", "Hello");
 
 //        TFloginUsername = (EditText) findViewById(R.id.TFloginUsername);
 //        TFloginPassword = (EditText) findViewById(R.id.TFloginPassword);
     }
 
     public void onClick(View v) {
-        if(v.getId() == R.id.Blogin) {
-                //check if username and password matches to value in database
-        }
         if(v.getId() == R.id.Bsignup) {
+                //check if username and password matches to value in database
+            Intent i = new Intent(Login.this, Signup.class);
+            startActivity(i);
+        }
+        if(v.getId() == R.id.Blogin) {
             EditText a = (EditText) findViewById(R.id.TFloginUsername);
             String strUser = a.getText().toString();
             EditText b = (EditText) findViewById(R.id.TFloginPassword);
             String strPass = b.getText().toString();
             Intent i = new Intent(Login.this, Home.class);
             i.putExtra("Username", strUser);
-            i.putExtra("Password", strPass);
+            i.putExtra("Password", strPass); //pass values
             startActivity(i);
         }
 
--- app/src/main/java/com/example/epantry/Signup.java
+++ app/src/main/java/com/example/epantry/Signup.java
@@ -1,8 +1,15 @@
+/**
+ * TODO:
+ * test method to make sure it creates new user with login if no match - assertNotNull
+ * test method that username field isn't empty
+ * test method that it checks password matches confirmed password
+ */
 package com.example.epantry;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,11 +25,25 @@ public class Signup extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        Log.i("Signup", "Hello");
 
         ETusername = (EditText) findViewById(R.id.TFname);
         ETemail = (EditText) findViewById(R.id.TFemail);
         ETpassword = (EditText) findViewById(R.id.TFpassword);
         ETconfirmpassword = (EditText) findViewById(R.id.TFconfirmpassword);
+    }
+
+    public boolean checkFilledTextFields(String username, String password) {
+        boolean valid;
+        if (findViewById(R.id.TFname) != null && findViewById(R.id.TFemail) != null
+                && findViewById(R.id.TFloginPassword) != null && findViewById(R.id.TFconfirmpassword) != null) {
+            valid = true;
+            Log.i("Signup", "true");
+        } else {
+            valid = false;
+            Log.i("Signup", "false");
+        }
+        return valid;
     }
 
     public void onSignupClick(View v) {
@@ -32,7 +53,7 @@ public class Signup extends Activity {
             String password = ETpassword.getText().toString();
             String confirmpassword = ETconfirmpassword.getText().toString();
 
-            if(!email.contains("@.com")) {
+            if (!email.contains("@.com")) {
                 Toast pass = Toast.makeText(Signup.this, "Email must be valid!", Toast.LENGTH_SHORT);
                 pass.show();
             }
@@ -40,9 +61,13 @@ public class Signup extends Activity {
                 //popup msg
                 Toast pass = Toast.makeText(Signup.this, "Passwords don't match!", Toast.LENGTH_SHORT);
                 pass.show();
-            } else if (password.equals(confirmpassword) && !password.isEmpty()) {
-                Intent i = new Intent(Signup.this, Home.class);
-                startActivity(i);
+            } else if (password.equals(confirmpassword) && !password.isEmpty() && checkFilledTextFields(username, email)) {
+
+                if (checkFilledTextFields(username, email)) {
+                    Log.i("Signup", "Confirmed login");
+                    Intent i = new Intent(Signup.this, Home.class);
+                    startActivity(i);
+                }
             }
         }
     }
